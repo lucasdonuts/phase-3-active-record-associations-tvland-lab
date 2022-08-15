0b24cc0aa2ae4507dea9675276e75f1294f34068
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-15 11:45:04 -0500

    commit tree 94460c7662e6bc048dcacc7a72f1c84bb5d9101d

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    self.characters.map do |char|
+      "#{char.actor.full_name}"
+    end
+  end
 end
